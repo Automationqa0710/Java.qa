f9e50492bb9fbb0d0a3667e4b87d67b2352d47da
Neto test .1 <khachikyan_r@mail.ru>
2024-05-27 16:02:40 +0400

    src/Main.java

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("2 + 3");
+        System.out.println(2 + 3);
+
+        int speed = 17;
+        int x = speed + 5;
+
+
+        double d = 6.5;
+        String name = " Petya ";
+
+        System.out.println(name + " was driving at speed " + speed);
+
+
+    }
+}
