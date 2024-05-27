b6e8cd14963e26a730e49ed69a3b556a3c57cb86
Neto test .1 <khachikyan_r@mail.ru>
2024-05-27 17:58:11 +0400

    Inital commit

--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,26 @@ public class Main {
         System.out.println(2 + 3);
 
         int speed = 17;
-        int x = speed + 5;
+
 
 
         double d = 6.5;
-        String name = " Petya ";
+        String name = "Petya";
 
         System.out.println(name + " was driving at speed " + speed);
+        int eaters = 5;
+
+        int water = 3000;
+        int potatoes = 5;
+        int chicken = 6;
+        int spices = 10;
+
+        System.out.println("Сварили суп. На одного человека вышло:");
+        System.out.println((water / eaters) + " миллилитров(а) воды");
+        System.out.println((potatoes / eaters) + " картофелин(а/ы)");
+        System.out.println((chicken / eaters) + " куриных(ое) бёдер(ро)");
+        System.out.println((spices / eaters) + " ложек(ки/ка) специй");
+
 
 
     }
